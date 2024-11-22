92145e5abca31ba43b778c87b66890964403fa10
Rabin Thapa <thapa.rabin7284.rt@gmail.com>
2024-11-22 16:33:07 -0600

    final

--- script.js
+++ script.js
@@ -4,9 +4,9 @@ function toggleMobileMenu(){
 
 
 // visitor counter for the number of people accessing the website
-async function fetchVisitorCount(){
+const counter = document.querySelector(".counter");
+async function updateCounter(){
     const response = await fetch("https://xy6l0ohu70.execute-api.us-east-1.amazonaws.com/cloud");
     const data = await response.json();
-    document.getElementById("views").innerHTML = data.count;
+    counter.innerHTML = 'views: ${data}'
 }
-fetchVisitorCount();
