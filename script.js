395f0ce75f86341f5fd4afec612e1d89800348f3
Rabin Thapa <thapa.rabin7284.rt@gmail.com>
2024-11-22 14:22:00 -0600

    java script

--- script.js
+++ script.js
@@ -2,3 +2,10 @@ function toggleMobileMenu(){
     document.getElementById("menu").classList.toggle("active");
 
 }
+async function fetchVisitorCount() {
+    const response = await fetch('https://xy6l0ohu70.execute-api.us-east-1.amazonaws.com/cloud');
+    const data = await response.json();
+    document.getElementById('visitor-count').innerText = data.count;
+}
+
+fetchVisitorCount();
